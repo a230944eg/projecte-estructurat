3cfaed95be8c72dcfcdb270359ce10bd1c59553a
a230944eg <a230944eg@h12.informatica.escoladeltreball.org>
2026-02-19 11:22:03 +0100

    Afegit càlcul del producte dels nombres 1-5

--- Main.java
+++ Main.java
@@ -2,23 +2,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Càlcul de la suma dels nombres primers de l'1 al 100
         int suma = 0;
-
         for (int i = 2; i <= 100; i++) {
-
             boolean esPrimer = true;
-
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     esPrimer = false;
                 }
             }
-
             if (esPrimer) {
                 suma = suma + i;
             }
         }
-
         System.out.println("Suma dels primers de l'1 al 100: " + suma);
+
+        // Segon càlcul: producte dels nombres de l'1 al 5
+        int producte = 1;
+        for (int i = 1; i <= 5; i++) {
+            producte = producte * i;
+        }
+        System.out.println("Producte de l'1 al 5: " + producte);
     }
 }
